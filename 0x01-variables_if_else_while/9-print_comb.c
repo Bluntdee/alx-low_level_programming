d5240c8b56a796f7d6a9ce95e715bdf53f37941b
Bluntdee <akintanoluwadarasimi14@gmail.com>
2023-08-11 14:00:10 -0700

    my first commit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int number;
+
+    for (number = 0; number <= 9; number++)
+    {
+        putchar(number + '0');
+
+        if (number < 9)
+        {
+            putchar(',');
+            putchar(' ');
+        }
+    }
+
+    putchar('\n');
+
+    return 0;
+}
+
