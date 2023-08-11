581ba1f88021369d05e7662a6d3bf52bb4839b6f
Bluntdee <akintanoluwadarasimi14@gmail.com>
2023-08-11 13:13:43 -0700

    my first commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char letter;
+
+    for (letter = 'a'; letter <= 'z'; letter++)
+    {
+        putchar(letter);
+    }
+
+    putchar('\n');
+
+    return 0;
+}
+
