cd2a8d8e1db6d0a884a825b97fc41574ff97ac4d
Bluntdee <akintanoluwadarasimi14@gmail.com>
2023-08-12 02:31:26 -0700

    compiler

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+/**
+* main - Entry point
+*
+* Return: 1 (Not Success)
+*/
+int main(void)
+{
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, quo, 59);
+	return (1);
+}
