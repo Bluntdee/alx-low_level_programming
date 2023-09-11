08e336055c04426370df847e0c9f325a57296690
Bluntdee <akintanoluwadarasimi14@gmail.com>
2023-09-11 05:59:04 -0700

    This is me

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,4 +1,12 @@
-#ifndef _HEADER_
-#define _HEADER_
-#define PI 3.14159265359
-#endif
+#include <stdio.h>
+
+/**
+ * main - prints the name of the file
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
