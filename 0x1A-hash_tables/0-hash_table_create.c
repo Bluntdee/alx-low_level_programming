77800a380ed3ce7013c6dcecd915692460396bd9
akintanoluwadarasimi <akintanoluwadarasimi14@gmail.com>
2023-12-23 05:19:08 -0800

    We are GenZ

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,27 +1,30 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - creates a hash table
- * @size: size of the array
- *
- * Return: pointer to the newly created hash table
- */
+* hash_table_create - Creates a Hash table.
+* @size: The size of array.
+*
+* Return: NULL If an error occurs.
+*         else - a pointer to the new hash table.
+*/
+
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *hash_table;
-	unsigned long int i;
+hash_table_t *table;
 
-	hash_table = malloc(sizeof(hash_table_t));
-	if (hash_table == NULL)
-		return (NULL);
-	hash_table->size = size;
-	hash_table->array = malloc(size * sizeof(hash_node_t *));
-	if (hash_table->array == NULL)
-	{
-		free(hash_table);
-		return (NULL);
-	}
-	for (i = 0; i < size; i++)
-		hash_table->array[i] = NULL;
-	return (hash_table);
+if (size == 0)
+return (NULL);
+
+table = calloc(1, sizeof(hash_table_t));
+if (table == NULL)
+return (NULL);
+
+table->size = size;
+table->array = calloc(size, sizeof(hash_node_t *));
+if (table->array == NULL)
+{
+free(table);
+return (NULL);
+}
+return (table);
 }
